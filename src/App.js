692139a4de349f747e432aa81f20b727b4e0747a
Adam Gardner <adga2012@gmail.com>
2022-07-06 23:24:06 -0600

    chore: add radio button fieldset add newsletter options to simulate radio buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Formik, Form } from "formik";
-import { Col, Container, Row } from "reactstrap";
+import { Col, Container, FormGroup, Row } from "reactstrap";
 import { Field, Submit } from "formik-strap";
 import { schema } from "./schema";
 import "./styles.css";
@@ -22,7 +22,8 @@ const App = () => (
         city: "",
         state: "",
         zip: "",
-        terms: false
+        terms: false,
+        newsletter: "no"
       }}
       validationSchema={schema}
       onSubmit={(values, { setSubmitting }) => {
@@ -33,62 +34,87 @@ const App = () => (
         }, 1500);
       }}
     >
-      <Form className="text-left">
-        <Row>
-          <Col md={4}>
-            <Field name="firstName" withLoading />
-          </Col>
-          <Col md={4}>
-            <Field name="lastName" withLoading />
-          </Col>
-          <Col md={4}>
-            <Field name="email" withLoading />
-          </Col>
-        </Row>
-        <Row>
-          <Col md={8}>
-            <Field name="username" withLoading />
-          </Col>
-          <Col md={4}>
-            <Field name="age" type="number" withLoading />
-          </Col>
-        </Row>
-        <Row>
-          <Field name="street1" withLoading />
-        </Row>
-        <Row>
-          <Field name="street2" withLoading />
-        </Row>
-        <Row>
-          <Col md={6}>
-            <Field name="city" withLoading />
-          </Col>
-          <Col md={2}>
-            <Field name="state" type="select" withLoading>
-              <option>---</option>
-              {(states ?? []).map((state) => (
-                <option value={state.name} key={state.abbreviation}>
-                  {state.abbreviation}
-                </option>
-              ))}
-            </Field>
-          </Col>
-          <Col md={4}>
-            <Field name="zip" withLoading />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Field
-              labelText="I accept the terms and conditions"
-              name="terms"
-              type="checkbox"
-              withLoading
-            />
-          </Col>
-        </Row>
-        <Submit withSpinner>Submit</Submit>
-      </Form>
+      {({ values }) => (
+        <Form className="text-left">
+          <Row>
+            <Col md={4}>
+              <Field name="firstName" withLoading />
+            </Col>
+            <Col md={4}>
+              <Field name="lastName" withLoading />
+            </Col>
+            <Col md={4}>
+              <Field name="email" withLoading />
+            </Col>
+          </Row>
+          <Row>
+            <Col md={8}>
+              <Field name="username" withLoading />
+            </Col>
+            <Col md={4}>
+              <Field name="age" type="number" withLoading />
+            </Col>
+          </Row>
+          <Row>
+            <Field name="street1" withLoading />
+          </Row>
+          <Row>
+            <Field name="street2" withLoading />
+          </Row>
+          <Row>
+            <Col md={6}>
+              <Field name="city" withLoading />
+            </Col>
+            <Col md={2}>
+              <Field name="state" type="select" withLoading>
+                <option>---</option>
+                {(states ?? []).map((state) => (
+                  <option value={state.name} key={state.abbreviation}>
+                    {state.abbreviation}
+                  </option>
+                ))}
+              </Field>
+            </Col>
+            <Col md={4}>
+              <Field name="zip" withLoading />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Field
+                labelText="I accept the terms and conditions"
+                name="terms"
+                type="checkbox"
+                withLoading
+              />
+            </Col>
+          </Row>
+          <Row>
+            <FormGroup tag="fieldset">
+              <Field
+                name="newsletter"
+                value="daily"
+                type="radio"
+                labelText="daily"
+              />
+              <Field
+                name="newsletter"
+                value="weekly"
+                type="radio"
+                labelText="weekly"
+              />
+              <Field
+                name="newsletter"
+                value="no"
+                type="radio"
+                labelText="No thanks"
+                checked={values.newsletter === "no"}
+              />
+            </FormGroup>
+          </Row>
+          <Submit withSpinner>Submit</Submit>
+        </Form>
+      )}
     </Formik>
   </Container>
 );
